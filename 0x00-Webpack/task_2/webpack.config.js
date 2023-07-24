35e645ec499dc65018c95876da4f4a7337989076
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-07-24 08:51:07 -0700

    Update

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -9,4 +11,35 @@ module.exports = {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+	use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+	type: 'asset/resource',
+	generator: {
+	  filename: 'assets/[name][ext]',
+	},
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
+    }),
+    new ImageMinimizerPlugin({
+      minimizerOptions: {
+        plugins: [
+          ['imagemin-webp', { quality: 75 }],
+          ['imagemin-svgo', { removeViewBox: false }],
+          ['imagemin-gifsicle', { optimizationLevel: 5 }],
+          ['imagemin-mozjpeg', { quality: 75, progressive: true }],
+          ['imagemin-pngquant', { quality: [0.6, 0.8] }],
+	],
+      },
+    }),
+  ],
 };
