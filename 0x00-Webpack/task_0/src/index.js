6e8fdf8295eefc1d45a50a48cc4e6053515b9957
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-07-24 02:25:57 -0700

    complete task_0

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,9 @@
+import $ from 'jquery';
+
+$(document).ready(function() {
+  const paragraph1 = $('<p>').text('Holberton Dashboard');
+  const paragraph2 = $('<p>').text('Dashboard data for the students');
+  const paragraph3 = $('<p>').text('Copyright - Holberton School');
+
+  $('body').append(paragraph1, paragraph2, paragraph3);
+});
