f59e794afe4390e3807fea9dfdd6d65de71e5e49
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-07-24 09:00:42 -0700

    update

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -4,25 +4,43 @@ const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
 
 module.exports = {
   mode: 'production',
-  entry: {
-    main: path.resolve(__dirname, './js/dashboard_main.js'),
-  },
+  entry: './js/dashboard_main.js',
   output: {
-    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-	use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.(png|jpe?g|gif|svg)$/i,
-	type: 'asset/resource',
-	generator: {
-	  filename: 'assets/[name][ext]',
-	},
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name][ext]',
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        use: [
+          {
+            loader: ImageMinimizerPlugin.loader,
+            options: {
+              severityError: 'warning', // (default)
+              minimizerOptions: {
+                plugins: [
+                  ['imagemin-webp', { quality: 75 }],
+                  ['imagemin-svgo', { removeViewBox: false }],
+                  ['imagemin-gifsicle', { optimizationLevel: 5 }],
+                  ['imagemin-mozjpeg', { quality: 75, progressive: true }],
+                  ['imagemin-pngquant', { quality: [0.6, 0.8] }],
+                ],
+              },
+            },
+          },
+        ],
       },
     ],
   },
@@ -30,16 +48,6 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: 'main.css',
     }),
-    new ImageMinimizerPlugin({
-      minimizerOptions: {
-        plugins: [
-          ['imagemin-webp', { quality: 75 }],
-          ['imagemin-svgo', { removeViewBox: false }],
-          ['imagemin-gifsicle', { optimizationLevel: 5 }],
-          ['imagemin-mozjpeg', { quality: 75, progressive: true }],
-          ['imagemin-pngquant', { quality: [0.6, 0.8] }],
-	],
-      },
-    }),
   ],
 };
+
