a8e6c430ee03cbb9c5682dea5e4ac441c4eda4c7
adeniyitobi055 <adedejiadeniyi055@gmail.com>
2023-07-25 20:12:58 +0100

    Update task 2

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -11,6 +11,17 @@ function App() {
       </header>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+        <section className='form-input'>
+          <section className='input'>
+            <label htmlFor='email'>Email: </label>
+            <input type='email' name='email' id='email' />
+          </section>
+          <section className='input'>
+            <label htmlFor='password'>Password: </label>
+            <input type='password' name='password' id='password' />
+          </section>
+          <button>OK</button>
+        </section>
       </div>
       <footer className="App-footer">
         <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import closeIcon from './close-icon.png';
 import './Notifications.css';
+import { getLatestNotification } from './utils';
 
 function Notifications() {
     return (
         <div className='Notifications'>
-            <p>Here is the list of notifications</p>
+            <p style={{ display: 'inline', marginRight: '80%' }}>
+                Here is the list of notifications
+            </p>
+            <button 
+                aria-label='Close'
+                onClick={console.log('Close button has been clicked')}
+            >
+                <img style={{ display: 'inline'}} src={closeIcon} alt='Close' />
+            </button>
+            <ul>
+                <li data='default'>New course available</li>
+                <li data='urgent'>New resume available</li>
+                <li 
+                    data='urgent'
+                    dangerouslySetInnerHTML={{__html: getLatestNotification() }}
+                >
+                </li>
+            </ul>
         </div>
     );
 }
