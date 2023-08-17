ce61383d7d19c5747cfc790f0eea68d333320b40
adeniyitobi055 <61655996+adeniyitobi055@users.noreply.github.com>
2023-08-17 08:37:14 +0100

    Update 8-seq.js

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -4,7 +4,7 @@ export default function printBestStudents(grades) {
   const bestStudent = Seq(grades)
     .filter((student) => student.score >= 70)
     .map((student) => ({
-      score: x.score,
+      ...student,
       firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
       lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
     }));
